065fa7da794532f230f2c24da31995791e20ea54
Leafpool <63614345+leafpool243@users.noreply.github.com>
2021-10-23 22:13:12 -0700

    Button interaction test
    When the command is run, it will reply with a message that has buttons. Only the person that sent the command will be able to interact with the buttons.

---
+++ commands/buttons.js
@@ -0,0 +1,88 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("buttons")
+        .setDescription("Send a message with buttons"),
+    async execute(interaction) {
+
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("button_yes")
+                    .setLabel("Yes")
+                    .setEmoji("✔")
+                    .setStyle("SUCCESS")
+            )
+            .addComponents(
+                new MessageButton()
+                    .setCustomId("button_no")
+                    .setLabel("No")
+                    .setEmoji("✖")
+                    .setStyle("DANGER")
+            );
+
+        const link = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setURL("https://google.com")
+                    .setLabel("test link to google")
+                    .setStyle("LINK")
+            );
+
+        await interaction.reply({
+            content: "**Example question**\nAre you sure you wish to continue? (*Yes* or *No*)",
+            components: [row, link],
+        });
+
+        const filter = i => {
+            if (i.user.id === interaction.user.id) return true;
+            return i.reply({
+                content: "Sorry, you cannot interact with this message because you did not send this command.",
+                ephemeral: true,
+            });
+        };
+
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter,
+            max: 1,
+            time: 1000 * 150,
+        });
+
+        collector.on("collect", async i => {
+
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId("button_yes")
+                        .setLabel("Yes")
+                        .setEmoji("✔")
+                        .setStyle("SUCCESS")
+                        .setDisabled(true)
+                )
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId("button_no")
+                        .setLabel("No")
+                        .setEmoji("✖")
+                        .setStyle("DANGER")
+                        .setDisabled(true)
+                );
+
+            interaction.editReply({
+                components: [row, link],
+            });
+
+            if (i.customId === "button_yes") {
+                i.reply({
+                    content: "You selected “yes”.",
+                });
+            } else if (i.customId === "button_no") {
+                i.reply({
+                    content: "You selected “no”.",
+                });
+            }
+        });
+    },
+};
