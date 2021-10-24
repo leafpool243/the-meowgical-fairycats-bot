a79ba69c4714f8420f6dcaf0e59abc28f1084597
Leafpool <63614345+leafpool243@users.noreply.github.com>
2021-10-23 23:05:05 -0700

    Added comma to come after name

--- commands/ni3hao3.js
+++ commands/ni3hao3.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName("你好")
         .setDescription("跟機器人說你好！！ :3"),
     async execute(interaction) {
-        await interaction.reply(`${interaction.member.nickname || interaction.user.username} 你好！:3`);
+        await interaction.reply(`${interaction.member.nickname || interaction.user.username}，你好！:3`);
     },
 };
