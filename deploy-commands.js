03ba25e56c37659ecfb874b9815e3e6a775152fd
Leafpool <63614345+leafpool243@users.noreply.github.com>
2021-10-24 22:56:21 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,13 +7,13 @@ const commands = [];
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: "9" }).setToken(token);
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
-	.then(() => console.log("Successfully registered application commands."))
+    .then(() => console.log("Successfully registered application commands."))
     .then(() => console.log(commands))
-	.catch(console.error);
+    .catch(console.error);
