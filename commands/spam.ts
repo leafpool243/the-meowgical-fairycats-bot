065ad198aa06f83b9b656d8d34014683746ac0ca
Leafpool <63614345+leafpool243@users.noreply.github.com>
2021-11-16 16:40:51 -0800

    Finally finished spam command

--- commands/spam.ts
+++ commands/spam.ts
@@ -4,17 +4,25 @@ const permissioncheck = require("../permissioncheck.ts")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("spam")
-        .setDescription("SPAM >:3"),
+        .setDescription("SPAM >:3")
+        .addIntegerOption(option => option
+            .setName("num_of_times")
+            .setDescription("The amount of times you want it to spam.")
+            .setRequired(true))
+        .addStringOption(option => option
+            .setName("message")
+            .setDescription("The message you want it to spam")
+            .setRequired(true)),
     async execute(interaction) {
         if (!permissioncheck.execute(interaction)) return;
 
         await interaction.reply({
-            content: "Starting chaos now.",
+            content: "Starting spam now.",
             ephemeral: true
         });
 
-        for(let i = 0; i < 5; i++) {
-            interaction.channel.send("meow");
+        for(let i = 0; i < interaction.options.getInteger("num_of_times"); i++) {
+            interaction.channel.send(interaction.options.getString("message"));
         }
     },
 };
