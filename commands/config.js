71232e6cdbe56b92a522414b669854d8ab4036c0
AmberWing <63614345+leafpool243@users.noreply.github.com>
2021-12-01 21:18:06 -0800

    Rebase from main

--- commands/config.js
+++ commands/config.js
@@ -1,6 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+const tester = require("../testersonly.js")
+>>>>>>> 372871d (Rebase from main)
 =======
 const tester = require("../testersonly.js")
 >>>>>>> 372871d (Rebase from main)
@@ -38,9 +42,12 @@ module.exports = {
                 .setName("view")
                 .setDescription("View current config")
 <<<<<<< HEAD
+<<<<<<< HEAD
         ),
     async execute(interaction) {
 =======
+=======
+>>>>>>> 372871d (Rebase from main)
             ),
     async execute(interaction) {
         const returnView = (column) => {
@@ -49,12 +56,16 @@ module.exports = {
             })
         }
 
+<<<<<<< HEAD
+>>>>>>> 372871d (Rebase from main)
+=======
 >>>>>>> 372871d (Rebase from main)
         if (interaction.options.getSubcommand() === "view") {
             const embed = new MessageEmbed()
                 .setColor("#9C59B6")
                 .setTitle("Current config")
                 .setDescription(`
+<<<<<<< HEAD
 <<<<<<< HEAD
 Test: ${JSON.stringify(db.prepare(`SELECT test FROM users WHERE userid=${interaction.user.id}`).get())}
                 `);
@@ -63,12 +74,17 @@ Test: ${JSON.stringify(db.prepare(`SELECT test FROM users WHERE userid=${interac
             db.run("UPDATE users SET test = ? WHERE userid = ?", [(interaction.options.getString("value") === "unset" ? undefined : interaction.options.getString("value")), interaction.user.id]);
             interaction.reply(`Option \`${interaction.options.getSubcommand()}\` has been set to \`${interaction.options.getString("value")}\`\n\nUnfortunately, the \`/config view\` command does not currently work, so currently, there is no way to see what value you currently have set.`);
 =======
+=======
+>>>>>>> 372871d (Rebase from main)
 Test: ${returnView("test")}
                 `);
             interaction.reply({ embeds: [embed] });
         } else if (interaction.options.getSubcommand() === "test") {
             db.run("UPDATE users SET test = ? WHERE userid = ?", [(interaction.options.getString("value") === "unset" ? undefined : interaction.options.getString("value")), interaction.user.id])
             interaction.reply(`Option \`${interaction.options.getSubcommand()}\` has been set to \`${interaction.options.getString("value")}\``);
+<<<<<<< HEAD
+>>>>>>> 372871d (Rebase from main)
+=======
 >>>>>>> 372871d (Rebase from main)
         }
     },
