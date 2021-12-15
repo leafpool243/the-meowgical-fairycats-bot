e10109ef344a54f893db5737a5211e6b9f484fbd
AmberWing <63614345+leafpool243@users.noreply.github.com>
2021-12-15 15:35:02 -0800

    Rebase from main

--- commands/config.js
+++ commands/config.js
@@ -1,5 +1,9 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
+<<<<<<< HEAD
+=======
+const tester = require("../testersonly.js")
+>>>>>>> 372871d (Rebase from main)
 const sqlite = require("sqlite3").verbose();
 let db = new sqlite.Database("database.db", sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
 
@@ -33,19 +37,39 @@ module.exports = {
             subcommand
                 .setName("view")
                 .setDescription("View current config")
+<<<<<<< HEAD
         ),
     async execute(interaction) {
+=======
+            ),
+    async execute(interaction) {
+        const returnView = (column) => {
+            db.get("SELECT ? FROM users WHERE userid = ?", column, interaction.user.id, (row) => {
+                return (!row ? "Not set" : row.column)
+            })
+        }
+
+>>>>>>> 372871d (Rebase from main)
         if (interaction.options.getSubcommand() === "view") {
             const embed = new MessageEmbed()
                 .setColor("#9C59B6")
                 .setTitle("Current config")
                 .setDescription(`
+<<<<<<< HEAD
 Test: ${JSON.stringify(db.prepare(`SELECT test FROM users WHERE userid=${interaction.user.id}`).get())}
                 `);
             interaction.reply({ embeds: [embed] });
         } else if (interaction.options.getSubcommand() === "test") {
             db.run("UPDATE users SET test = ? WHERE userid = ?", [(interaction.options.getString("value") === "unset" ? undefined : interaction.options.getString("value")), interaction.user.id]);
             interaction.reply(`Option \`${interaction.options.getSubcommand()}\` has been set to \`${interaction.options.getString("value")}\`\n\nUnfortunately, the \`/config view\` command does not currently work, so currently, there is no way to see what value you currently have set.`);
+=======
+Test: ${returnView("test")}
+                `);
+            interaction.reply({ embeds: [embed] });
+        } else if (interaction.options.getSubcommand() === "test") {
+            db.run("UPDATE users SET test = ? WHERE userid = ?", [(interaction.options.getString("value") === "unset" ? undefined : interaction.options.getString("value")), interaction.user.id])
+            interaction.reply(`Option \`${interaction.options.getSubcommand()}\` has been set to \`${interaction.options.getString("value")}\``);
+>>>>>>> 372871d (Rebase from main)
         }
     },
 };
